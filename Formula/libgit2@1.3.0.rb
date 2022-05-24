11dde6943b9447e519fda3a02dcf15076cd52020
K B Sarat Chandra <sarat@rafay.co>
2022-05-24 15:27:02 -0700

    adding libgit2 1.3.0

---
+++ Formula/libgit2@1.3.0.rb
@@ -0,0 +1,55 @@
+class Libgit2AT130 < Formula
+  desc "C library of Git core methods that is re-entrant and linkable"
+  homepage "https://libgit2.github.com/"
+  url "https://github.com/libgit2/libgit2/archive/v1.3.0.tar.gz"
+  sha256 "192eeff84596ff09efb6b01835a066f2df7cd7985e0991c79595688e6b36444e"
+  license "GPL-2.0-only"
+  head "https://github.com/libgit2/libgit2.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libssh2"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_EXAMPLES=YES"
+    args << "-DBUILD_CLAR=NO" # Don't build tests.
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      cd "examples" do
+        (pkgshare/"examples").install "lg2"
+      end
+      system "make", "clean"
+      system "cmake", "..", "-DBUILD_SHARED_LIBS=OFF", *args
+      system "make"
+      lib.install "libgit2.a"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <git2.h>
+
+      int main(int argc, char *argv[]) {
+        int options = git_libgit2_features();
+        return 0;
+      }
+    EOS
+    libssh2 = Formula["libssh2"]
+    flags = %W[
+      -I#{include}
+      -I#{libssh2.opt_include}
+      -L#{lib}
+      -lgit2
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
